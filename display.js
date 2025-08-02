312cec84ed5cd5e6d9e4c33f2eccfd7add393af5
Madhukar B <tejasbondre27@gmail.com>
2025-08-02 21:02:09 +0530

    final fe commit

--- display.js
+++ display.js
@@ -42,7 +42,7 @@ const showUrls = (urls) => {
    
 <div class="bg-white shadow-md rounded-lg p-4 my-4 mx-auto w-[90%] max-w-3xl">
     <div class=" flex flex-col md:flex-row md:items-center md:justify-between gap-2"  > 
-    <p class="text-sm text-gray-700"> <strong class="text-indigo-600"> Short:</strong> <a href="${shortUrl}" target="_blank" rel="noopener noreferrer >${shortUrl}</a></p>
+    <p class="text-sm text-gray-700"> <strong class="text-indigo-600"> Short:</strong> <a href="${shortUrl}" target="_blank" rel="noopener noreferrer" >${shortUrl}</a></p>
     <p class="text-sm text-gray-700 break-all overflow-hidden"> <strong class="text-indigo-600"> Long:</strong> <a href="${longUrl}"  target="_blank" rel="noopener noreferrer" >${longUrl}</a></p>
     </div>
 </div>
