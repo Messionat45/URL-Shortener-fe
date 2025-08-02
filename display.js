459122d78b5cab8583818a67ce84bb6205e06f80
Madhukar B <tejasbondre27@gmail.com>
2025-08-02 13:32:04 +0530

    completed ui for url page

--- display.js
+++ display.js
@@ -28,7 +28,8 @@ const showUrls = (urls) => {
   displayUrlsDiv.innerHTML = "";
 
   if (urls.length === 0) {
-    displayUrlsDiv.innerHTML = "<p> NO URL's Present";
+    displayUrlsDiv.innerHTML =
+      "<p class='text-center text-gray-600 text-xl'>No URLs present</p>";
     return;
   }
 
@@ -37,9 +38,12 @@ const showUrls = (urls) => {
     const shortUrl = `${beUrl}/s/${urlObject.shorturl}`;
 
     const block = `
-    <div> 
-    <p> <strong> Short:</strong> <a href="${shortUrl}" >${shortUrl}</a></p>
-    <p> <strong> Long:</strong> <a href="${longUrl}" >${longUrl}</a></p>
+   
+<div class="bg-white shadow-md rounded-lg p-4 my-4 mx-auto w-[90%] max-w-3xl">
+    <div class=" flex flex-col sm-flex-row sm:items-center sm:justify-between gap-2"  > 
+    <p class="text-sm text-gray-700"> <strong class="text-indigo-600"> Short:</strong> <a href="${shortUrl}" >${shortUrl}</a></p>
+    <p class="text-sm text-gray-700"> <strong class="text-indigo-600"> Long:</strong> <a href="${longUrl}" >${longUrl}</a></p>
+    </div>
 </div>
     `;
 
