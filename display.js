6387b8ffe5e08cb9340561ea266bd1e1e98ebbba
Madhukar B <tejasbondre27@gmail.com>
2025-08-02 13:34:28 +0530

    completed ui for url page

--- display.js
+++ display.js
@@ -40,7 +40,7 @@ const showUrls = (urls) => {
     const block = `
    
 <div class="bg-white shadow-md rounded-lg p-4 my-4 mx-auto w-[90%] max-w-3xl">
-    <div class=" flex flex-col sm-flex-row sm:items-center sm:justify-between gap-2"  > 
+    <div class=" flex flex-col md-flex-row md:items-center md:justify-between gap-2"  > 
     <p class="text-sm text-gray-700"> <strong class="text-indigo-600"> Short:</strong> <a href="${shortUrl}" >${shortUrl}</a></p>
     <p class="text-sm text-gray-700"> <strong class="text-indigo-600"> Long:</strong> <a href="${longUrl}" >${longUrl}</a></p>
     </div>
